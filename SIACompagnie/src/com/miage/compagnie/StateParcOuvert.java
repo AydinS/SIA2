4f1101c0ff76838c32ad74ae72eb068436c71238
simaydin <simaydin@miage21a14.dmiage.u-paris10.fr>
2016-03-24 17:43:21 +0100

    Client test des design pattern

--- SIACompagnie/src/com/miage/compagnie/StateParcOuvert.java
+++ SIACompagnie/src/com/miage/compagnie/StateParcOuvert.java
@@ -5,12 +5,12 @@ public class StateParcOuvert implements StateParcAttraction {
 	@Override
 	public void gererParcAttraction(Parc_attraction parc) {
 		// TODO Auto-generated method stub
-		System.out.println("Le parc"+ parc.getName() + "est ouvert!");
-		System.out.println("Venez essayer nos" + parc.getNbAttraction() + "attraction(s)!");
+		System.out.println("Le parc "+ parc.getName() + " est ouvert!");
+		System.out.println("Venez essayer nos " + parc.getNbAttraction() + " attraction(s)!");
 		System.out.println("50€ l'entrée! 25€ pour les enfants!");
 		for(int i = 0;i<4;i++){
 			int nbEntree = (i == 0 ? parc.getEntree() : parc.getEntree()*i);
-			System.out.println("Semaine "+i+": nb Entree:"+ nbEntree);
+			System.out.println("Semaine "+i+": nb Entree: "+ nbEntree);
 			parc.setEntree(nbEntree);
 			parc.setNbVisitor((int) (nbEntree*1.5));
 		}
